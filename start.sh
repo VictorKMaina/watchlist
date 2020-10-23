477c2dfb95170057337430de5881495fb38784a8
sophieO1970 <sophieO1970@gmail.com>
2020-10-23 11:43:00 +0300

    added files and make changes

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
-export MOVIE_API_KEY=ad94be3aa93348ce805d3a3da4885839
-export SECRET_KEY=sophieCee
-
+export MOVIE_API_KEY='ad94be3aa93348ce805d3a3da4885839'
+export SECRET_KEY='sophieCee'
+export MAIL_USERNAME='sophieolondie@gmail.com'
+export MAIL_PASSWORD='adhiambok12345'
 
 python3.8 manage.py server
